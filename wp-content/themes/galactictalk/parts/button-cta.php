2ff3f74010cf02747d27e7c2afaa9000d20f96e6
osgsm <50955473+osgsm@users.noreply.github.com>
2025-01-05 15:34:12 +0900

    Add button-cta.php

---
+++ wp-content/themes/galactictalk/parts/button-cta.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * CTA button
+ *
+ * @package GalacticTalk
+ */
+
+$defaults = array(
+	'class' => '',
+);
+$args     = wp_parse_args( $args, $defaults );
+?>
+
+<a class="<?php cx( 'grid h-80 grid-cols-[auto,1fr] grid-rows-[5rem] items-center rounded-full mx-auto max-w-640 lg:max-w-none bg-[#F748A2] pl-0 pr-28 tracking-[-0.02em] lg:h-120 lg:grid-cols-[repeat(3,auto)] lg:grid-rows-[7.5rem] lg:pl-48 lg:pr-0', $args['class'] ); ?>" href="#">
+	<div class="col-[1/2] row-[1/2] h-96 self-end overflow-hidden rounded-bl-40 lg:col-auto lg:row-auto lg:h-160 lg:w-196 lg:rounded-bl-0">
+		<img class="w-108 lg:w-220" src="<?php echo esc_url( get_theme_file_uri( '/assets/images/tutor-whole-3.webp' ) ); ?>" alt="" width="220" height="315">
+	</div>
+	<div class="flex items-center justify-between -ml-16 gap-12 font-bold leading-none text-white ~text-15/24">
+		<span class="leading-normal break-keep"><span class="~text-16/32">14日間トライアル</span><wbr>に申し込む</span>
+		<span class="grid shrink-0 place-items-center rounded-full bg-white text-[#F748A2] ~size-32/40 before:icon-chevron-right before:size-16"></span>
+	</div>
+	<div class="relative col-[1/2] row-[1/2] grid w-130 place-items-center *:col-span-full *:row-span-full lg:col-auto lg:row-auto lg:w-248">
+		<img class="hidden lg:block w-42 translate-x-[2rem] translate-y-[-0.5rem] rotate-[24deg] scale-x-[-1] lg:w-108 lg:translate-x-[-2rem] lg:translate-y-[-1.5rem]" src="<?php echo esc_url( get_theme_file_uri( '/assets/images/headphone.webp' ) ); ?>" alt="" width="108" height="136">
+		<img class="hidden lg:block w-18 translate-y-[1.25rem] rotate-[-19deg] lg:w-50 lg:translate-x-[3rem] lg:translate-y-[0.5rem]" src="<?php echo esc_url( get_theme_file_uri( '/assets/images/crystal-yellow.webp' ) ); ?>" alt="" width="50" height="78">
+	</div>
+</a>
