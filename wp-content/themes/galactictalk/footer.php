4a8c4c12c9de3d9a785c45f9cbd7c26412c11615
osgsm <50955473+osgsm@users.noreply.github.com>
2025-01-21 17:07:43 +0900

    Add animation for footer

--- wp-content/themes/galactictalk/footer.php
+++ wp-content/themes/galactictalk/footer.php
@@ -21,21 +21,19 @@
 ">
 	<div class="container relative z-10 grid gap-50 pt-20 ~pb-72/244">
 		<div class="grid items-center gap-16 *:col-span-full *:row-span-full">
-			<div class="leading-[1.04] tracking-[-0.03em] ~text-50/132 stroke-text *:inline-block">
-				<span>The Power </span><span>of Language</span>
-			</div>
-			<div class="font-bold leading-normal ~text-10/28 ~px-1/4 ~pt-6/14 gradient-text">
-				ワクワクする未来を話そう
-			</div>
+			<p class="js-split-text leading-[1.04] tracking-[-0.03em] ~text-50/132 stroke-text *:inline-block">
+				<span class="sr-only">The Power of Language</span>
+				<?php split_text( 'The Power' ); ?>
+				<?php split_text( 'of Language' ); ?>
+			</p>
+			<p class="font-bold leading-normal ~text-10/28 ~px-1/4 gradient-text">ワクワクする未来を話そう</p>
 		</div>
 		<div class="flex w-[13rem] flex-col ~gap-40/80 lg:w-auto lg:flex-row lg:items-start">
 			<div class="grid max-w-[18.625rem] place-items-start gap-[1.6875rem]">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<img class="~h-38/54 w-auto" src="<?php echo esc_url( get_theme_file_uri( '/assets/images/logo.svg' ) ); ?>" alt="<?php bloginfo( 'name' ); ?>">
+					<img class="~h-38/54 w-full" src="<?php echo esc_url( get_theme_file_uri( '/assets/images/logo.svg' ) ); ?>" alt="<?php bloginfo( 'name' ); ?>" width="297" height="54">
 				</a>
-				<div class="break-keep leading-normal">
-					東京都近未来区1-2-3 ギャラクシービル102F
-				</div>
+				<div class="break-keep leading-normal">東京都近未来区1-2-3 ギャラクシービル102F</div>
 			</div>
 			<nav>
 				<?php
@@ -51,24 +49,32 @@
 			</nav>
 		</div>
 	</div>
-	<?php
-	foreach ( array(
-		array(
-			'class'  => '-bottom-8 ~/lg:~left-[10.875rem]/[16.5rem] ~/lg:~w-260/360 lg:left-[63vw] lg:top-0 lg:w-[28.875rem]',
-			'src'    => '/assets/images/footer-1.webp',
-			'width'  => 462,
-			'height' => 653,
-		),
-		array(
-			'class'  => '~/lg:~top-120/200 ~/lg:~left-480/680 ~/lg:~w-220/320 lg:-left-[4.1875rem] lg:bottom-0 lg:top-388 lg:w-280',
-			'src'    => '/assets/images/footer-2.webp',
-			'width'  => 280,
-			'height' => 395,
-		),
-	) as $image ) :
-		?>
-		<img class="<?php cx( 'absolute rounded-t-full ~rounded-b-[2.5rem]/[6rem]', $image['class'] ); ?>" src="<?php echo esc_url( get_theme_file_uri( $image['src'] ) ); ?>" alt="" width="<?php echo esc_attr( $image['width'] ); ?>" height="<?php echo esc_attr( $image['height'] ); ?>">
-	<?php endforeach; ?>
+	<div class="js-staggered pointer-events-none absolute inset-0">
+		<?php
+		foreach ( array(
+			array(
+				'class'  => '-bottom-8 ~/lg:~left-200/264 ~/lg:~w-260/360 lg:left-[63vw] lg:top-0 lg:w-[28.875rem]',
+				'src'    => '/assets/images/footer-1.webp',
+				'width'  => 462,
+				'height' => 653,
+			),
+			array(
+				'class'  => '~/lg:~top-120/200 ~/lg:~left-480/680 ~/lg:~w-220/320 lg:-left-[4.1875rem] lg:bottom-0 lg:top-388 lg:w-280',
+				'src'    => '/assets/images/footer-2.webp',
+				'width'  => 280,
+				'height' => 395,
+			),
+		) as $image ) :
+			?>
+			<img
+				class="<?php cx( 'absolute rounded-t-full ~rounded-b-[2.5rem]/[6rem]', $image['class'] ); ?>"
+				src="<?php echo esc_url( get_theme_file_uri( $image['src'] ) ); ?>"
+				alt=""
+				width="<?php echo esc_attr( $image['width'] ); ?>"
+				height="<?php echo esc_attr( $image['height'] ); ?>"
+			>
+		<?php endforeach; ?>
+	</div>
 </footer>
 
 <?php wp_footer(); ?>
