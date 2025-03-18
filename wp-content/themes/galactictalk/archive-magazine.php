f4e2e0054c41c597dd77cf1f261dfb56bc19c447
Tomomi <torano.manji@gmail.com>
2025-03-18 18:31:22 +0900

    Add / Magazine mv

--- wp-content/themes/galactictalk/archive-magazine.php
+++ wp-content/themes/galactictalk/archive-magazine.php
@@ -7,6 +7,20 @@
 
 get_header(); ?>
 
+<!-- TODO: メインビジュアルコンポーネントを作成する -->
+<div class="relative mb-24 grid *:col-span-full *:row-span-full lg:mb-0">
+	<div class="relative ~/lg:~h-400/600 overflow-hidden lg:h-[min(calc(100vw*0.75),37.5rem)]">
+		<img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/thumbnail_magazine.jpg' ) ); ?>" alt="" class='h-full w-full object-cover' />
+		<div class="absolute inset-0 bg-gradient-to-b from-transparent from-10% to-black lg:from-40%"></div>
+	</div>
+	<div class="z-10 container grid justify-self-end items-start ~mt-196/224 lg:pb-142">
+		<div class="grid font-barlow uppercase">
+			<p class="font-bold leading-[1.2] ~text-40/120">Magazine</p>
+			<h1 class="mt-8/0 w-fit bg-brand-600 leading-relaxed lg:leading-none ~text-14/18 ~rounded-2/4 font-bold ~p-4/8">マガジン</h1>
+		</div>
+	</div>
+</div>
+
 <?php if ( have_posts() ) : ?>
 
 <div class="container grid ~gap-x-16/32 ~gap-y-40/80 lg:grid-cols-3">
@@ -19,7 +33,7 @@ get_header(); ?>
 
 		<div class="">
 			<a href="<?php echo esc_url( get_the_permalink() ); ?>" class="flex flex-col h-fit *:shrink-0 ~/xl:~rounded-16/24">
-				<div class="aspect-[328/218] ~/xl:~rounded-16/24 overflow-hidden">
+				<div class="aspect-[352/198] overflow-hidden rounded-16">
 					<img
 						class="size-full object-cover"
 						src="<?php echo esc_url( get_the_post_thumbnail_url() ); ?>"
@@ -29,7 +43,7 @@ get_header(); ?>
 					>
 				</div>
 				<div class="grid grow auto-rows-min items-start gap-12 mt-16">
-					<h3 class="font-black ~text-15/20">
+					<h3 class="font-bold ~text-15/20 leading-normal">
 						<?php echo esc_html( get_the_title() ); ?>
 					</h3>
 					<?php if ( $tags ) : ?>
