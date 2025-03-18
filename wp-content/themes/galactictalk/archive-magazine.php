0d0913bae5b854b2830b25aee0c8214f4c1bb94a
Tomomi <torano.manji@gmail.com>
2025-03-18 18:34:21 +0900

    Improve / Magazine mv style

--- wp-content/themes/galactictalk/archive-magazine.php
+++ wp-content/themes/galactictalk/archive-magazine.php
@@ -13,7 +13,7 @@ get_header(); ?>
 		<img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/thumbnail_magazine.jpg' ) ); ?>" alt="" class='h-full w-full object-cover' />
 		<div class="absolute inset-0 bg-gradient-to-b from-transparent from-10% to-black lg:from-40%"></div>
 	</div>
-	<div class="z-10 container grid justify-self-end items-start ~mt-196/224 lg:pb-142">
+	<div class="z-10 container grid justify-self-end items-start ~mt-196/224">
 		<div class="grid font-barlow uppercase">
 			<p class="font-bold leading-[1.2] ~text-40/120">Magazine</p>
 			<h1 class="mt-8/0 w-fit bg-brand-600 leading-relaxed lg:leading-none ~text-14/18 ~rounded-2/4 font-bold ~p-4/8">マガジン</h1>
