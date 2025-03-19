290f5799727e4a813b387552a5445e405781859f
Tomomi <torano.manji@gmail.com>
2025-03-19 10:07:13 +0900

    Add / mv todo

--- wp-content/themes/galactictalk/archive-magazine.php
+++ wp-content/themes/galactictalk/archive-magazine.php
@@ -9,17 +9,9 @@ get_header(); ?>
 
 
 <!-- TODO: メインビジュアル確定したらコーディングする -->
-<div class="relative mb-24 grid *:col-span-full *:row-span-full lg:mb-0">
-	<div class="relative ~/lg:~h-400/600 overflow-hidden lg:h-[min(calc(100vw*0.75),37.5rem)]">
-		<img src="<?php echo esc_url( get_theme_file_uri( '/assets/images/thumbnail_magazine.jpg' ) ); ?>" alt="" class='h-full w-full object-cover' />
-		<div class="absolute inset-0 bg-gradient-to-b from-transparent from-10% to-black lg:from-40%"></div>
-	</div>
-	<div class="z-10 container grid justify-self-end items-start ~mt-196/224">
-		<div class="grid font-barlow uppercase">
-			<p class="font-bold leading-[1.2] ~text-40/120">Magazine</p>
-			<h1 class="mt-8/0 w-fit bg-brand-600 leading-relaxed lg:leading-none ~text-14/18 ~rounded-2/4 font-bold ~p-4/8">マガジン</h1>
-		</div>
-	</div>
+<div>
+	<p class="uppercase">Magazine</p>
+	<h1>マガジン</h1>
 </div>
 
 <?php if ( have_posts() ) : ?>
