e11023c25ba4173d7aeb468073a33d1271c07fc9
Tomomi <torano.manji@gmail.com>
2025-04-14 17:53:31 +0900

    Add / Tutor archive page, page header images

---
+++ wp-content/themes/galactictalk/archive-tutor.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Archive Template
+ *
+ * @package GalacticTalk
+ */
+
+get_header();
+?>
+
+<div>
+	<?php
+	get_template_part(
+		'parts/page-header',
+		null,
+		array(
+			'title'    => 'Tutors',
+			'subtitle' => '講師紹介',
+			'image'    => 'mv-tutors.webp',
+		)
+	);
+	?>
+
+	<?php if ( have_posts() ) : ?>
+		<div class="container grid ~gap-x-16/32 ~gap-y-40/80 lg:grid-cols-3">
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				?>
+				<?php
+				get_template_part(
+					'parts/tutor-card',
+					null,
+				);
+				?>
+			<?php endwhile; ?>
+		</div>
+		<?php
+	endif;
+	?>
+</div>
+
+<?php
+get_footer();
