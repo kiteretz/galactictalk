c9852a78225d7ece0f3bdd2db45f850386929644
Kite <ixkaito@gmail.com>
2025-09-19 23:05:51 +0900

    Fix modal

--- wp-content/themes/galactictalk/src/ts/modules/modal.ts
+++ wp-content/themes/galactictalk/src/ts/modules/modal.ts
@@ -4,9 +4,15 @@
  */
 import MicroModal from 'micromodal';
 
+MicroModal.init();
+
 const triggers = document.querySelectorAll('.js-modal-trigger');
 triggers.forEach((trigger) => {
-  const target = trigger.getAttribute('href')?.match(/^#(.*)/)?.[1];
+  const a = trigger.matches('a')
+    ? trigger
+    : trigger.querySelector('a[href^="#"]');
+
+  const target = a?.getAttribute('href')?.match(/^#(.*)/)?.[1];
 
   if (!target) {
     return;
@@ -18,7 +24,9 @@ triggers.forEach((trigger) => {
     return;
   }
 
-  trigger.addEventListener('click', () => {
+  trigger.addEventListener('click', (e) => {
+    e.preventDefault();
+
     if (targetElement.getAttribute('aria-hidden') === 'true') {
       MicroModal.show(target);
     } else {
