df4ebc7e29ead6ded65e52ead9a565ce6b11a688
Kite <ixkaito@gmail.com>
2025-09-19 16:31:11 +0900

    Improve modal

--- wp-content/themes/galactictalk/parts/modal.php
+++ wp-content/themes/galactictalk/parts/modal.php
@@ -7,41 +7,55 @@
 
 ?>
 
-<div id="modal-overlay" class="
-<?php
-	cx(
-		'overlay',
-		'fixed inset-0 w-full h-full bg-black/50 z-[1000] transition-opacity duration-300 ease-[ease] opacity-0 pointer-events-none',
-		'aria-[hidden=false]:opacity-100 aria-[hidden=false]:pointer-events-auto'
-	)
-	?>
-" aria-hidden="true"></div>
 <div id="thankyou" class="
 <?php
 	cx(
-		'modal',
-		'fixed opacity-0 top-1/2 left-1/2 w-full max-w-752 max-h-[100vh] px-16 -translate-x-1/2 translate-y-[-60%] pointer-events-none z-[1001] transition-all duration-300 ease-[ease]',
-		'aria-[hidden=false]:opacity-100 aria-[hidden=false]:pointer-events-auto aria-[hidden=false]:-translate-x-1/2 aria-[hidden=false]:-translate-y-1/2',
+		'modal group/modal',
+		'fixed inset-0 size-full grid place-items-center px-16 z-[1001]',
+		'transition-all duration-300 ease-out',
+		'aria-hidden:opacity-0 aria-hidden:invisible',
 	)
 	?>
-" role="dialog" aria-labelledby="Thank you for Visiting" aria-hidden="true">
-	<div class="
-	<?php
-		cx(
-			'relative grid ~gap-y-20/32 bg-white px-16 ~/lg:~pt-40/64 ~/lg:~pb-40/56 rounded-24 shadow-md overflow-y-auto transition-all duration-300 ease-[ease]',
-			'[.active_&]:opacity-100 [.active_&]:pointer-events-auto'
-		)
-		?>
-	">
-		<h2 class="text-[#07030E] font-barlow font-bold uppercase text-center ~text-48/64 leading-none tracking-[-0.02em]">
-		Thank you<br />
-		for Visiting
+" aria-hidden="true">
+	<div
+		class="
+			<?php
+			cx(
+				'relative grid ~gap-y-20/32 w-full max-w-752 bg-white px-16 ~/lg:~pt-40/64 ~/lg:~pb-40/56 rounded-24 shadow-md overflow-y-auto',
+				'transition duration-300 ease-out',
+				'group-aria-hidden/modal:opacity-0 group-aria-hidden/modal:translate-y-32'
+			)
+			?>
+		"
+		role="dialog"
+		aria-modal="true"
+		aria-labelledby="thankyou-title"
+	>
+		<h2
+			id="thankyou-title"
+			class="text-[#07030E] font-barlow font-bold uppercase text-center ~text-48/64 leading-none tracking-[-0.02em]"
+			aria-hidden="true"
+		>
+			<span class="sr-only">Thank you for Visiting</span>
+			Thank you
+			<br />
+			for Visiting
 		</h2>
 		<div class="grid ~gap-y-12/16 text-black ~text-15/16 text-center leading-loose">
-			<p>GalacticTalk は、地球にある制作会社<br />
-			株式会社キテレツが制作した架空の宇宙語スクールです 🚀</p>
-			<p>このサイトの企画からデザイン、実装など一連のプロセスを体験できる講座を<br />
-			オンライン教育サービス Coloso にて公開中！</p>
+			<p>
+				GalacticTalk は、地球にある制作会社
+				<br />
+				株式会社キテレツが制作した
+				<br />
+				架空の宇宙語スクールです 🚀
+			</p>
+			<p>
+				このサイトの企画からデザイン・実装など
+				<br />
+				一連のプロセスを体験できる講座を
+				<br />
+				オンライン教育サービス Coloso にて公開中！
+			</p>
 		</div>
 		<?php
 		button(
@@ -56,17 +70,34 @@
 			)
 		);
 		?>
-		<button class="
-		<?php
-			cx(
-				'js-modal-trigger modal-close',
-				'~/lg:~size-20/24 absolute ~/lg:~top-24/40 ~/lg:~right-24/40'
-			)
+		<button
+			class="
+				<?php
+				cx(
+					'~/lg:~size-20/24 absolute ~/lg:~top-24/40 ~/lg:~right-24/40'
+				)
+				?>
+			"
+			aria-label="モーダルを閉じる"
+			data-micromodal-close
+		>
+			<?php
+			// SVG アイコンの部分をクリックすると反応しないことがあるため、`pointer-events-none` を追加.
 			?>
-		" aria-label="モーダルを閉じる">
-			<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none" viewBox="0 0 26 26" class="size-full">
+			<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none" viewBox="0 0 26 26" class="size-full pointer-events-none">
 				<path stroke="#000" d="m1 1 24 24M1 25 25 1"/>
 			</svg>
 		</button>
 	</div>
+	<div
+		class="
+			<?php
+			cx(
+				'overlay',
+				'fixed inset-0 size-full bg-black/50 -z-10',
+			)
+			?>
+		"
+		data-micromodal-close
+	></div>
 </div>
--- wp-content/themes/galactictalk/src/ts/modules/modal.ts
+++ wp-content/themes/galactictalk/src/ts/modules/modal.ts
@@ -1,51 +1,28 @@
 /**
  * Modal
  *
- * .js-modal-trigger Open/close modal (#thankyou) on element click in class
  */
+import MicroModal from 'micromodal';
 
 const triggers = document.querySelectorAll('.js-modal-trigger');
-const overlay = document.querySelector('.overlay');
-const modal = document.getElementById('thankyou');
-
-// スクロールバーの幅を取得
-function getScrollberWidth() {
-  return window.innerWidth - document.documentElement.clientWidth;
-}
-
-// モーダル表示/非表示
-function setModalVisibility(visible: boolean) {
-  if (!modal || !overlay) return;
-  modal.setAttribute('aria-hidden', visible ? 'false' : 'true');
-  overlay.setAttribute('aria-hidden', visible ? 'false' : 'true');
-
-  // モーダル表示時にスクロールバー非表示分の余白を追加
-  if (visible) {
-    const scrollberWidth = getScrollberWidth();
-    document.body.style.overflow = 'hidden';
-    document.body.style.paddingRight = `${scrollberWidth}px`;
-  } else {
-    document.body.style.overflow = '';
-    document.body.style.paddingRight = '';
-  }
-}
-
-// .js-modal-trigger クリックでモーダルを開く
 triggers.forEach((trigger) => {
-  trigger.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (!modal || !overlay) return;
-    const isOpen = modal.getAttribute('aria-hidden') === 'false';
-    setModalVisibility(!isOpen);
+  const target = trigger.getAttribute('href')?.match(/^#(.*)/)?.[1];
+
+  if (!target) {
+    return;
+  }
+
+  const targetElement = document.getElementById(target);
+
+  if (!targetElement) {
+    return;
+  }
+
+  trigger.addEventListener('click', () => {
+    if (targetElement.getAttribute('aria-hidden') === 'true') {
+      MicroModal.show(target);
+    } else {
+      MicroModal.close(target);
+    }
   });
-});
-
-// .overlay クリックでモーダルを閉じる
-overlay?.addEventListener('click', (e) => {
-  if (e.target === overlay) setModalVisibility(false);
-});
-
-// ESC キーでモーダルを閉じる
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') setModalVisibility(false);
 });
